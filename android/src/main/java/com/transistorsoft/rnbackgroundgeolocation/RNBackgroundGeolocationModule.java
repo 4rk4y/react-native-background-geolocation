130c2796366f7e15158f46f70a458cbcea17a32d
jaewnoha <jwh@t2nters.com>
2019-12-25 20:57:31 +0900

    cont

--- android/src/main/java/com/transistorsoft/rnbackgroundgeolocation/RNBackgroundGeolocationModule.java
+++ android/src/main/java/com/transistorsoft/rnbackgroundgeolocation/RNBackgroundGeolocationModule.java
@@ -1,5 +1,8 @@
 package com.transistorsoft.rnbackgroundgeolocation;
 
+import android.app.Activity;
+import android.content.Intent;
+
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -13,4 +16,34 @@ public class RNBackgroundGeolocationModule extends ReactContextBaseJavaModule im
         super(reactContext);
     }
 
+    @Override
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+
+    }
+
+    @Override
+    public void onHostResume() {
+
+    }
+
+    @Override
+    public void onHostPause() {
+
+    }
+
+    @Override
+    public void onHostDestroy() {
+
+    }
+
+    @Nonnull
+    @Override
+    public String getName() {
+        return null;
+    }
 }
